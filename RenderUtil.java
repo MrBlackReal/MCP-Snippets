3e40fc7b5dc0f9420186e5af6e5682dbd65ee261
MrBlackReal <61763141+MrBlackReal@users.noreply.github.com>
2022-08-19 18:03:06 +0700

    Update RenderUtil.java

--- RenderUtil.java
+++ RenderUtil.java
@@ -2,23 +2,27 @@ package dev.mrblackreal.client.util;
 
 import org.lwjgl.opengl.GL11;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.shader.Framebuffer;
 import net.minecraft.client.shader.ShaderGroup;
+import net.minecraft.util.ResourceLocation;
 
 public class RenderUtil {
 
-    private static int lastScaleFactor;
+	private static final Minecraft mc = Minecraft.getMinecraft();
+
+	private static int lastScaleFactor;
 	private static int lastScaleWidth;
 	private static int lastScaleHeight;
 	private static ResourceLocation shader = new ResourceLocation("shaders/post/blur.json");
 	private static ShaderGroup blurShader;
 	private static Framebuffer buffer;
 
-    private static void setShaderConfig(float intensity, float blurWidth, float blurHeight) {
+	private static void setShaderConfig(float intensity, float blurWidth, float blurHeight) {
 		blurShader.getShaders().get(0).getShaderManager().getShaderUniform("Radius").set(intensity);
 		blurShader.getShaders().get(1).getShaderManager().getShaderUniform("Radius").set(intensity);
 
@@ -26,7 +30,7 @@ public class RenderUtil {
 		blurShader.getShaders().get(1).getShaderManager().getShaderUniform("BlurDir").set(blurHeight, blurWidth);
 	}
 
-    public static void initFboAndShader(double d, int categoryY, int i, int j, float f) {
+	public static void initFboAndShader(double d, int categoryY, int i, int j, float f) {
 		try {
 			blurShader = new ShaderGroup(mc.getTextureManager(), mc.getResourceManager(), mc.getFramebuffer(), shader);
 			blurShader.createBindFramebuffers(mc.displayWidth, mc.displayHeight);
@@ -37,11 +41,11 @@ public class RenderUtil {
 		}
 	}
 
-    public static void drawBluredRect(float x, float y, float width, float height, float intensity) {
+	public static void drawBluredRect(float x, float y, float width, float height, float intensity) {
 		final ScaledResolution sr = new ScaledResolution(mc);
 
 		intensity = Math.max(intensity, 1);
-		
+
 		int scaleFactor = sr.getScaleFactor();
 		int scaledWidth = sr.getScaledWidth();
 		int scaledHeight = sr.getScaledHeight();
@@ -62,14 +66,14 @@ public class RenderUtil {
 			setShaderConfig(intensity, 1, 0);
 			buffer.bindFramebuffer(true);
 			blurShader.loadShaderGroup(mc.timer.renderPartialTicks);
-			
+
 			mc.getFramebuffer().bindFramebuffer(true);
 
 			GL11.glDisable(GL11.GL_SCISSOR_TEST);
 			GlStateManager.enableBlend();
 			GlStateManager.tryBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ZERO, GL11.GL_ONE);
 			GlStateManager.disableBlend();
-			
+
 			GL11.glScalef(scaleFactor * .5f, scaleFactor * .5f, 0);
 		}
 	}
